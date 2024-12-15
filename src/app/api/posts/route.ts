fce2fe70535c98d566056a735f02d33bf1fab075
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-12-15 04:44:54 +0000

    GETguest post

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,27 +1,13 @@
-import {getAllPosts} from "@/src/actions/guestPost"
+import {getallPosts} from "@/src/actions/guestPost"
 import { auth } from "@/src/lib/auth"
 import { NextResponse } from "next/server"
-type Post = {
-    id: String
-    title: String
-    body: String
-    createdAt: String // Change type from Date to string
-    updatedAt: String // Change type from Date to string
-    username: String
-    discordId: String
-}
 
 // todo get all posts from database and sort into a json object with the structure of [id, title, body, createdAt, updatedAt, username, discordId] - all strings
-export async function GET() {
-    return auth().then(async (user) => {
-        if (!user) {
-            return NextResponse.redirect("/login")
-        }
-        const posts = await getAllPosts()
-        return new Response(JSON.stringify(posts), {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-    })
-}
+export const GET = auth(function GET(req) {
+    const posts = getallPosts()
+    if (!posts) return NextResponse.json({ error: "No posts found" }, { status: 404 })
+    
+    if (req.auth) return NextResponse.json(posts)
+
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 })
+})
