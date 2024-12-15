fa5bbd718a4ea5341d68cb37ec04ee9d305fc8a7
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-12-15 04:46:14 +0000

    get all guestbook posts

--- src/components/GuestBook.tsx
+++ src/components/GuestBook.tsx
@@ -1,30 +1,34 @@
 
-import { getAllPosts } from "@/src/actions/guestPost";
+import { getallPosts } from "@/src/actions/guestPost";
 import Link from "next/link";
 import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "@/src/components/ui/card";
 import { Button } from "@/src/components/ui/button";
-import { Post } from "@/src/types/posts";
 
 
 export default async function PostsList() {
   // post types
-  const posts = await getAllPosts() as Post[];
+  const posts = await getallPosts();
   
+  const bodtTextLength = 100;
+  posts.forEach((post) => {
+    if (post.body.length > bodtTextLength) {
+      post.body = post.body.substring(0, bodtTextLength) + '...';
+    }
+  });
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => (
-        <Link key={post.id} href={`/guestbook/${post.id}`}>
-          <Card className="max-w-sm w-full sm:w-1/2 lg:w-full justify-around items-center flex flex-col rounded-md ring-1 ring-inset bg-zinc-900/25 text-zinc-800 ring-zinc-400/25 dark:bg-zinc-900/25 dark:text-zinc-400 dark:ring-zinc-400/25 hover:text-zinc-400 dark:hover:text-zinc-400">
+          <Card key={post.id} className="max-w-sm w-full sm:w-1/2 lg:w-full justify-around items-center flex flex-col rounded-md ring-1 ring-inset bg-zinc-900/25 text-zinc-800 ring-zinc-400/25 dark:bg-zinc-900/25 dark:text-zinc-400 dark:ring-zinc-400/25 hover:text-zinc-400 dark:hover:text-zinc-400">
             <CardHeader>
               <CardTitle className="text-center justify-center items-center text-2xl text-primary-300 font-bold">
                 {post.title}
               </CardTitle>
             </CardHeader>
             <CardContent className="flex flex-col justify-center items-center gap-2">
-              <span className="text-center justify-center items-center text-lg text-gray-800 dark:text-gray-200">
+              <CardDescription>
                 {post.body}
-              </span>
+              </CardDescription>
             </CardContent>
             <CardFooter className="flex flex-col justify-center gap-2">
               <span className="text-center justify-center items-center text-gray-800 dark:text-gray-200">
@@ -40,7 +44,6 @@ export default async function PostsList() {
               </div>
             </CardFooter>
           </Card>
-        </Link>
       ))}
     </div>
   );
