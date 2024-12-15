2a3e1260789afa77bbcfff91444fe01f4f5a35fd
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-12-15 06:49:48 +0000

    next.config.js changes added image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: ['cdn.tomdev.xyz'],
+    },
 }
 
 module.exports = nextConfig
