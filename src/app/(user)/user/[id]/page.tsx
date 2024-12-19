9bba67afb199f2d17bcfee708b990778d74a6e31
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-12-19 00:52:43 +0000

    comment out discord badges for now

--- src/app/(user)/user/[id]/page.tsx
+++ src/app/(user)/user/[id]/page.tsx
@@ -33,31 +33,31 @@ type User = {
   emailVerified: boolean | null;
 };
 
-export const DiscordBadges = (flag: number): string[] => {
-  let flags: string[] = [];
+// export const DiscordBadges = (flag: number): string[] => {
+//   let flags: string[] = [];
 
-  // In the order they appear on profiles
-  if (flag & 1) flags.push("Discord_Employee"); // 1 << 0
-  if (flag & 262144) flags.push("Discord_Certified_Moderator"); // 1 << 18
-  if (flag & 2) flags.push("Partnered_Server_Owner"); // 1 << 1
-  if (flag & 4) flags.push("hypesquad"); // 1 << 2
-  if (flag & 64) flags.push("House_Bravery"); // 1 << 6
-  if (flag & 128) flags.push("House_Brilliance"); // 1 << 7
-  if (flag & 256) flags.push("House_Balance"); // 1 << 8
-  if (flag & 8) flags.push("Bug_Hunter_Level_1"); // 1 << 3
-  if (flag & 16384) flags.push("Bug_Hunter_Level_2"); // 1 << 14
-  if (flag & 4194304) flags.push("Active_Developer"); // 1 << 22
-  if (flag & 131072) flags.push("Early_Verified_Bot_Developer"); // 1 << 17
-  if (flag & 512) flags.push("Early_Supporter"); // 1 << 9
-  if (flag & 1024) flags.push("Team_User"); // 1 << 10
-  if (flag & 2048) flags.push("System"); // 1 << 11
-  if (flag & 4096) flags.push("Bug_Hunter_Level_3"); // 1 << 12
-  if (flag & 8192) flags.push("Verified_Bot"); // 1 << 13
-  if (flag & 65536) flags.push("Verified_Bot_Developer"); // 1 << 16
-  if (flag & 524288) flags.push("Early_Supporter"); // 1 << 19
+//   // In the order they appear on profiles
+//   if (flag & 1) flags.push("Discord_Employee"); // 1 << 0
+//   if (flag & 262144) flags.push("Discord_Certified_Moderator"); // 1 << 18
+//   if (flag & 2) flags.push("Partnered_Server_Owner"); // 1 << 1
+//   if (flag & 4) flags.push("hypesquad"); // 1 << 2
+//   if (flag & 64) flags.push("House_Bravery"); // 1 << 6
+//   if (flag & 128) flags.push("House_Brilliance"); // 1 << 7
+//   if (flag & 256) flags.push("House_Balance"); // 1 << 8
+//   if (flag & 8) flags.push("Bug_Hunter_Level_1"); // 1 << 3
+//   if (flag & 16384) flags.push("Bug_Hunter_Level_2"); // 1 << 14
+//   if (flag & 4194304) flags.push("Active_Developer"); // 1 << 22
+//   if (flag & 131072) flags.push("Early_Verified_Bot_Developer"); // 1 << 17
+//   if (flag & 512) flags.push("Early_Supporter"); // 1 << 9
+//   if (flag & 1024) flags.push("Team_User"); // 1 << 10
+//   if (flag & 2048) flags.push("System"); // 1 << 11
+//   if (flag & 4096) flags.push("Bug_Hunter_Level_3"); // 1 << 12
+//   if (flag & 8192) flags.push("Verified_Bot"); // 1 << 13
+//   if (flag & 65536) flags.push("Verified_Bot_Developer"); // 1 << 16
+//   if (flag & 524288) flags.push("Early_Supporter"); // 1 << 19
 
-  return flags;
-};
+//   return flags;
+// };
 
 
 
@@ -85,7 +85,7 @@ export default async function MeProfilePage({ params }: { params: { id: string }
   //   nitroType = false && user.premium_type === 0;
   // }
 
-  let flags: string[] = DiscordBadges(user.public_flags);
+  // let flags: string[] = DiscordBadges(user.public_flags);
   if (user.avatar && user.avatar.includes("a_")) flags.push("Nitro");
 
   let userBanner = user.banner;
